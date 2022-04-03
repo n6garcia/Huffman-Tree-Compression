d29b3cd82f90521e02da431f7a7fa6fd7a93111a
Noel Garcia <45529400+n6garcia@users.noreply.github.com>
2022-04-03 02:25:03 -0700

    Update BitInputStream.cpp

--- src/bitStream/input/BitInputStream.cpp
+++ src/bitStream/input/BitInputStream.cpp
@@ -1,8 +1,5 @@
 #include "BitInputStream.hpp"
 
-/**
- * TODO: Fill the buffer of size bufSize from the input stream.
- */
 void BitInputStream::fill() {
     char c;
     int i;
@@ -20,21 +17,6 @@ void BitInputStream::fill() {
     byteCap = i;
 }
 
-/**
- * TODO: Return true when the istream tried to read more bytes than the
- * remaining bytes in the file and the buffer was also fully read. Otherwise,
- * return false.
- *
- * Note that eof() is different from this function. eof() in most other istreams
- * is only true when we are trying to read past the end of the file. Whereas
- * this function should be only true when we are at the end of the file, but
- * have not read past it yet. For example:
- *
- * // some code to read all the data in the file
- * atEndOfFile(); // atEndOfFile() = true. But eof() is still false for now.
- * readBit();     // trying to read past the end of the file
- * eof();         // eof is true now
- */
 bool BitInputStream::atEndOfFile() {
     if (nbits == ((byteCap * 8) - padding) && in.eof()) {
         return true;
@@ -49,16 +31,6 @@ bool BitInputStream::eof() { return eofBit; }
 /* Returns true when trying to read past the end of the file, false otherwise */
 void BitInputStream::setPadding(unsigned int n) { padding = n; }
 
-/**
- * TODO: Read the next bit from the bit buffer. Fills the buffer from the input
- * stream if all the bits have already been read. It should return 0 if the bit
- * read is 0, and return 1 if the bit read is 1.
- *
- * Before trying to read a bit from the buffer, if atEndOFfile() is true, set
- * the eofBit and directly return 0 (You can return any number here, since after
- * setting eofBit to true, eof() will return true and the program won’t be using
- * the returned value from readBit).
- */
 unsigned int BitInputStream::readBit() {
     // check for EOF
     if (atEndOfFile()) {

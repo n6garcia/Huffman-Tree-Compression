a21e58a568518059e8153e12008fc7629f218335
Noel Garcia <45529400+n6garcia@users.noreply.github.com>
2022-04-03 02:23:48 -0700

    Update HCTree.cpp

--- src/encoder/HCTree.cpp
+++ src/encoder/HCTree.cpp
@@ -11,7 +11,6 @@ void deserializeTree(BitInputStream& bis, HCNode*& node, HCNode* parent);
 void deserializeLeaves(stringstream& ssl, HCNode* node,
                        vector<HCNode*>& leaves);
 
-/* TODO: Delete all objects on the heap to avoid memory leaks. */
 HCTree::~HCTree() { deleteNode(root); }
 
 string HCTree::serialize() {
@@ -140,24 +139,6 @@ void printNode(HCNode* node) {
     printNode(node->c1);
 }
 
-/**
- * TODO: Build the HCTree from the given frequency vector. You can assume
- * the vector must have size 256 and each value at index i represents the
- * frequency of char with ASCII value i. Only non-zero frequency symbols
- * should be used to build the tree. The leaves vector must be updated so
- * that it can be used in encode() to improve performance.
- *
- * When building the HCTree, you should use the following tie-breaking rules
- * to match the output from reference solution in checkpoint:
- *
- *    1. HCNode with lower count should have higher priority. If count is
- * the same, then HCNode with a larger ascii value symbol should have higher
- * priority. (This should be already defined properly in the comparator in
- * HCNode.hpp)
- *    2. When popping two highest priority nodes from PQ, the higher
- * priority node will be the ‘c0’ child of the new parent HCNode.
- *    3. The symbol of any parent node should be taken from its 'c0' child.
- */
 void HCTree::build(const vector<unsigned int>& freqs) {
     // init PQ (forest)
     priority_queue<HCNode*, vector<HCNode*>, HCNodePtrComp> PQ;
@@ -219,14 +200,6 @@ void updateLeaves(HCNode* n, vector<HCNode*>& leaves) {
     if (n->c1) updateLeaves(n->c1, leaves);
 }
 
-/**
- * TODO: Write the encoding bits of the given symbol to the ostream. You
- * should write each encoding bit as ascii char either '0' or '1' to the
- * ostream. You must not perform a comprehensive search to find the encoding
- * bits of the given symbol, and you should use the leaves vector instead to
- * achieve efficient encoding. For this function to work, build() must be
- * called before to create the HCTree.
- */
 void HCTree::encode(byte symbol, BitOutputStream& out) const {
     vector<char> rev_encoding;
     HCNode* n;
@@ -267,14 +240,6 @@ void HCTree::encode(byte symbol, BitOutputStream& out) const {
     }
 }
 
-/**
- * TODO: Write the encoding bits of the given symbol to ostream. You should
- * write each encoding bit as ascii char either '0' or '1' to the ostream.
- * You must not perform a comprehensive search to find the encoding bits of
- * the given symbol, and you should use the leaves vector instead to achieve
- * efficient encoding. For this function to work, build() must have been
- * called beforehand to create the HCTree.
- */
 void HCTree::encode(byte symbol, ostream& out) const {
     vector<char> rev_encoding;
     HCNode* n;
@@ -316,11 +281,6 @@ void HCTree::encode(byte symbol, ostream& out) const {
     }
 }
 
-/**
- * TODO: Decode the sequence of bits (represented as a char of either '0' or
- * '1') from the istream to return the coded symbol. For this function to
- * work, build() must have been called beforehand to create the HCTree.
- */
 byte HCTree::decode(BitInputStream& in) const {
     HCNode* n = root;
     char c;
@@ -351,11 +311,6 @@ byte HCTree::decode(BitInputStream& in) const {
     return n->symbol;
 }
 
-/**
- * TODO: Decode the sequence of bits (represented as char of either '0' or
- * '1') from istream to return the coded symbol. For this function to work,
- * build() must have been called beforehand to create the HCTree.
- */
 byte HCTree::decode(istream& in) const {
     HCNode* n = root;
     char c;
